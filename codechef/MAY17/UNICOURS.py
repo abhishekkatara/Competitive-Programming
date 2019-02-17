b60995d4f4eb3859b97a36d3109082cb084dd3d1
AbhishekKatara <abhikatara.sanskar91@gmail.com>
2019-02-17 16:37:53 +0530

    Solutions of this year updated

--- codechef/MAY17/UNICOURS.py
+++
@@ -1,9 +0,0 @@
-for test in range(int(raw_input())):
-	n = int(raw_input())
-	temp = 0
-	arr = [int(x) for x in raw_input().split()]
-	for j in range(len(arr)):
-	if temp<arr[j]:
-	temp = arr[j]
-	 
-	print (n-temp)
